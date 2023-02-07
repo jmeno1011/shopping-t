2bcb7b5af41feef91423c94bd24157691bc1b548
jmeno1011 <jwhltn8282@gmail.com>
2023-02-08 02:02:36 +0900

    add frame

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,11 +9,44 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1 className="text-3xl font-bold underline text-slate-500">
-          Hello world
-        </h1>
-      </main>
+      <div className="md:container md:mx-auto">
+        <header className="bg-[#35BCB2]">
+          {/* logo, login / join */}
+          <div className="p-4 flex justify-between items-baseline mb-4 border-solid border-0 border-b-[1px] border-slate-800">
+            <h1 className="text-3xl font-bold underline text-slate-900">
+              shopping-t
+            </h1>
+            <div className="flex gap-2">
+              <a>로그인</a>
+              <a>회원가입</a>
+            </div>
+          </div>
+          {/* search / profile / cart */}
+          <div className="px-4 flex justify-between">
+            <input type="text" />
+            <div className="flex gap-2">
+              <a>개인정보</a>
+              <a>장바구니</a>
+            </div>
+          </div>
+          {/* nav */}
+          <nav className="px-4 py-2">
+            <ul className="flex gap-2">
+              <li>전체</li>
+              <li>카테고리1</li>
+              <li>카테고리2</li>
+            </ul>
+          </nav>
+        </header>
+        {/* carousel */}
+        <div className="p-8">
+          <div className="bg-slate-400 h-[350px] flex items-center justify-center">
+            carousel
+          </div>
+        </div>
+        {/* items */}
+        <div className="bg-green-100 p-4">items</div>
+      </div>
     </>
   );
 }
