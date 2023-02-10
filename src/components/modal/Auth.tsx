3019fceaa4359065e11ba2022edc4e575e795617
jmeno1011 <jwhltn8282@gmail.com>
2023-02-10 17:57:35 +0900

    adjust github svg scale

--- src/components/modal/Auth.tsx
+++ src/components/modal/Auth.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 export default function Auth() {
   return (
     <div>
-        <button className="mb-2 w-[180px] flex items-center bg-white">
+        <button className="mb-2 w-[180px] flex items-center bg-white h-[32px]">
             <Image className="mr-2" src={"/auth_svg/btn_google_light_normal_ios.svg"} alt="google-login" height={32} width={32} />
             <span className="text-sm inline-block ">Sign in with Google</span>
         </button>
-        <button className="mb-2 w-[180px] flex items-center bg-white">
-            <Image className="mr-2" src={"/auth_svg/github-mark.svg"} alt="github-login" height={32} width={32}/>
+        <button className="mb-2 w-[180px] flex items-center bg-white h-[32px]">
+            <Image className="mr-2 p-2" src={"/auth_svg/github-mark.svg"} alt="github-login" height={32} width={32} />
             <span className="text-sm inline-block ">Sign in with Github</span>
         </button>        
     </div>
