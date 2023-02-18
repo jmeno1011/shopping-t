42d5865b1abac85821473f45039c312c6bb3363b
jmeno1011 <jwhltn8282@gmail.com>
2023-02-18 17:31:08 +0900

    add imagefiles

--- src/components/home/categories/CategoryRow.tsx
+++ src/components/home/categories/CategoryRow.tsx
@@ -1,25 +1,34 @@
 import Image from 'next/image'
 import React from 'react'
 
-const categoryList = ["옷", "신발", "뷰티", "가구", "주방용품", "폰"]
+const categoryList = [
+    { name: "옷", imgUrl: "/categoryRow/cloth.svg" },
+    { name: "신발", imgUrl: "/categoryRow/shoes.svg" },
+    { name: "뷰티", imgUrl: "/categoryRow/cosmetics.jpg" },
+    { name: "가구", imgUrl: "/categoryRow/furniture.jpg" },
+    { name: "주방용품", imgUrl: "/categoryRow/kitchen.jpg" },
+    { name: "폰", imgUrl: "/categoryRow/phone.svg" }
+]
 
 export default function CategoryRow() {
     return (
         <div className='flex'>
             {
                 categoryList.map(category => (
-                    <div key={category} className="flex flex-col grow bg-white">
+                    <div key={category.name} className="flex flex-col grow bg-white">
                         <div>
                             <Image
-                                src="/categoryRow/cloth.svg"
+                                src={category.imgUrl}
                                 alt="#"
-                                width={700}
-                                height={475}
-                                sizes="100vw"
-                                className='w-full h-auto'
-                                />
+                                width={190}
+                                height={190}
+                                fill
+                                style={{objectFit:'contain'}}
+                                // sizes="100vw"
+                                // className='w-full h-auto'
+                            />
                         </div>
-                        <h5 className='p-1 text-center'>{category}</h5>
+                        <h5 className='p-1 text-center'>{category.name}</h5>
                     </div>
                 ))
             }
